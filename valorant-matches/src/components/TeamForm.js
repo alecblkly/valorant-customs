b61bfffcdca4e99c35306b6943621d96191a5a28
Alec Blakeley <alecblkly@gmail.com>
2020-06-30 16:02:58 -0400

    Added base comments for teamselect and teamform.

---
+++ valorant-matches/src/components/TeamForm.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+
+function TeamForm() {}
+
+export default TeamForm;
---
+++ valorant-matches/src/components/TeamSelect.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+
+function TeamSelect() {}
+
+export default TeamSelect;
